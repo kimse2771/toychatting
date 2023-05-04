97f053ff95ecb74902b3634c0dc962829a7012c2
kse3917 <kws3917@naver.com>
2023-05-04 09:54:36 +0900

    init 0504

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:toychat/main_pg.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage()
+    );
+  }
+}
+
+
---
+++ lib/main_pg.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  bool isLoginButton = false;
+
+
+  _login(){
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+              top: 200,
+              child: Text(
+                  "bubble chat"),
+            ),
+
+            Positioned(
+              top: 400,
+              left: 150,
+
+              child: !isLoginButton?
+              OutlinedButton(
+                child: Text("로그인하기"),
+                onPressed: (){
+                  setState(() {
+                    isLoginButton = true;
+                  });
+                  },
+              ) : OutlinedButton(
+                  onPressed: (){
+                    setState(() {
+                      isLoginButton = false;
+                    });
+                    },
+                  child: Text("버튼 바꾸기"),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
